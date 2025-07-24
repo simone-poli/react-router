ae792ec16c63f4fefc5c25413c815cdc0dad0f55
simone-poli <polismn97@gmail.com>
2025-07-24 14:08:05 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import * as bootstrap from 'bootstrap'
+
+createRoot(document.getElementById('root')).render(
+  
+    <App />
+
+)
